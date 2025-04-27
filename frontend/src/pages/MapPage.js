21594658235a1c4a8c46c6fa6213e904dab3c79b
Dhwanil Ranpura <dranpura1907@gmail.com>
2025-04-27 00:44:48 -0700

    updated backend

--- frontend/src/pages/MapPage.js
+++ frontend/src/pages/MapPage.js
@@ -3,7 +3,7 @@ import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import './MapPage.css';
 
-mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
+mapboxgl.accessToken = "pk.eyJ1IjoiZGh3YW5pbDE5MDciLCJhIjoiY205eXdvbWVzMWl0ODJscHZ1YWswa3VybyJ9.ifIhgY8CvD7JAD7Ug4MlxA";
 
 
 function MapPage() {
